eda6788e0bb2444e69101994ba7a4fffec41be47
James Carlson <jxxcarlson@mac.com>
2018-12-24 07:56:39 -0500

    Add sustainability, etc.

--- src/FlowModel.hs
+++ src/FlowModel.hs
@@ -1,34 +1,78 @@
-module FlowModel(efficiencyOfEdge) where
+module FlowModel(
+    sustainability, efficiency, resilience, alpha
+  ) where
 
 import SimpleGraph(
        SimpleGraph,SGEdge
       , sourceNode, targetNode
-      , flowOnEdge, outFlow)
+      , flowOnEdge, outFlow, inFlow, totalFlow)
+
+import Data.Graph.Inductive.Graph (labEdges)
+
+sustainability :: SimpleGraph -> Float
+sustainability graph =
+    let
+        a = alpha graph
+        aa = power a 1.288
+    in
+        -1.844 * aa * (logBase 2 aa)
+
+
+
+efficiency :: SimpleGraph -> Float
+efficiency graph =
+    let
+        totalFlow_ =
+            totalFlow graph
+
+        edges =
+            labEdges graph
+    in
+        sum(map (efficiencyOfEdge totalFlow_ graph) edges)
+
+resilience :: SimpleGraph -> Float
+resilience graph =
+    let
+        totalFlow_ =
+            totalFlow graph
+
+        edges =
+            labEdges graph
+    in
+        sum(map (resilienceOfEdge totalFlow_ graph) edges)
+
+
+alpha :: SimpleGraph -> Float
+alpha graph =
+    let
+        ratio =
+            1 + ((resilience graph) / (efficiency graph))
+    in
+        1 / ratio
 
 
 log2 :: Float -> Float
 log2 x = (log x)/(log 2)
 
+power :: Float -> Float -> Float
+power a x = exp(x * (log a))
+
 efficiencyOfEdge :: Float -> SimpleGraph -> SGEdge -> Float
 efficiencyOfEdge totalFlow_ graph edge =
   let
     ef = flowOnEdge edge
-    denominator = (outFlow graph (sourceNode edge)) * (outFlow graph (targetNode edge))
+    denominator = (outFlow graph (sourceNode edge)) * (inFlow graph (targetNode edge))
     numerator = ef * totalFlow_
     logRatio = log2 (numerator/denominator)
   in
     ef * logRatio
 
--- efficiencyOfEdge : Float -> SimpleGraph -> Edge -> Float
--- efficiencyOfEdge totalFlow_ graph edge =
---     let
---         denominator =
---             (SG.outFlow edge.from graph) * (SG.inFlow edge.to graph)
---
---         numerator =
---             edge.label.flow * totalFlow_
---
---         logRatio =
---             (logBase 2) (numerator / denominator)
---     in
---         roundTo 3 (edge.label.flow * logRatio)
+resilienceOfEdge :: Float -> SimpleGraph -> SGEdge -> Float
+resilienceOfEdge totalFlow_ graph edge =
+    let
+      ef = flowOnEdge edge
+      denominator = (outFlow graph (sourceNode edge)) * (inFlow graph (targetNode edge))
+      numerator = ef * ef
+      logRatio = log2 (numerator/denominator)
+    in
+      ef * logRatio
